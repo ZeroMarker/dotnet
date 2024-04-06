c78b4163d5385378190fbe5b3472af265dd03383
ZeroMarker <u78k4l67@163.com>
2024-04-07 00:31:27 +0800

    Auto commit

---
+++ Sharp/GUID.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharp
+{
+    internal class GUID
+    {
+        static void Main()
+        {
+            // Generate a new GUID
+            Guid newGuid = Guid.NewGuid();
+
+            // Print the generated GUID
+            Console.WriteLine("Generated GUID: " + newGuid);
+        }
+    }
+}

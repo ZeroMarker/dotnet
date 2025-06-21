e2442a3427c529daba0171de5adbbed82860d2f2
Mark Chen <u78k4l67@163.com>
2025-06-22 01:12:34 +0800

    delete

---
+++ Function/Program.fs
@@ -0,0 +1,33 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Hello from F#"
+
+let x = 4
+let y = 5
+
+let sum = x + y
+
+
+// record
+
+type Person = { Name: string; Age: int }
+
+let person = { Name = "Alice"; Age = 30 }
+
+// function
+
+let add a b = a + b
+
+let number = add x y
+
+let add25 = add 25
+
+let add100 = add25 100 // curried function
+
+
+// ## functions as first-class citizens
+let opNum x1 y1 = x1 y1
+
+let result = opNum (+) 25
+
+let res = result
+
